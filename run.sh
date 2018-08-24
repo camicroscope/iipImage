c7ce0646328630f2332dd6864437ec18ef97a865
Ryan Birmingham <birm@rbirm.us>
2018-08-24 17:04:03 -0400

    fix run cms

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build . -t iipauth
-docker run -d -p 4010:4010 -v "$(pwd)"/images/:/images/ iipauth
+docker build . -t iipsrv
+docker run -d -p 80:4010 -v "$(pwd)"/images/:/images/ iipsrv
